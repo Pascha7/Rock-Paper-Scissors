c799bd990e5fe092f8606ee9adaeddb0fca8c419
Muhammed-Ali Dogan <muhammedalidoganhh03@gmail.com>
2023-05-09 15:09:38 +0200

    Positioned 'return playerChoice;' outside of the while loop and created playRound and game function

--- index.js
+++ index.js
@@ -16,6 +16,33 @@ function playerSelection() {
   while (!validOptions.includes(playerChoice)) {
     // initializing playerChoice with user input
     playerChoice = prompt("Rock, Paper or Scissors?: ").toLowerCase();
-    return playerChoice;
   }
+  return playerChoice;
+}
+
+// Function for playing 1 round
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tie!";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Scissor";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissor beats Paper";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock beats Scissor";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissor beats Paper";
+  }
+}
+
+// Function for starting the game
+function game() {
+  let plChoice = playerSelection();
+  let CpChoice = getComputerChoice();
+  let result = playRound(plChoice, CpChoice);
+  console.log(result);
 }
